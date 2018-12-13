147e136c630f634b31ea2cb66b836a6daa816c81
Matt Sweeney <msweeney777@gmail.com>
2018-12-13 18:18:38 -0500

    Second commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const cities = require('cities');
+
+const myCity = cities.zip_lookup(03824);
+
+console.log(myCity);
--- printMessages.js
+++
@@ -1,5 +0,0 @@
-const messageModule = require('./messages');
-
-messageModule.messages.forEach((m) => {
-  console.log(m);
-});
